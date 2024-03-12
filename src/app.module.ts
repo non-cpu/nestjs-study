0555f9b07f8adcd60444e4d14f1f03f3f54eb2a0
non <grapher285@gmail.com>
2024-03-12 15:38:35 +0900

    Implement board CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/boards/boards.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { randomUUID } from 'crypto';
+import { CreateBoardDto } from './create-board.dto';
+
+@Injectable()
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+
+        const board: Board = {
+            id: randomUUID(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
+    }
+
+    getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.boards.find((board) => board.id === id);
+        board.status = status;
+        return board;
+    }
+}
